7ec43d08add0ecfd7c4cd2d9c2deb310463f1b4e
Chris Britton (RD-US) <chris_britton@trendmicro.com>
2022-12-04 01:46:26 -0600

    Refactor day 2 to use AbstractSolver

--- src/main/python/02.py
+++ src/main/python/02.py
@@ -4,19 +4,15 @@ Day 2: Rock Paper Scissors.
 
 https://adventofcode.com/2022/day/2
 """
+import os
 from dataclasses import dataclass
+from typing import Any
 
-from src.main.python.util import Timer
+from src.main.python.util import AbstractSolver
 
 
 @dataclass
 class Shape:
-    """
-    A shape (rock, paper, or scissors) in the "Rock, Paper, Scissors" game.
-
-    Attributes:
-        value: The value for the shape. A = rock, B = paper, C = scissors.
-    """
     value: str
 
     def is_rock(self) -> bool:
@@ -54,13 +50,6 @@ class Shape:
 
 @dataclass
 class Turn:
-    """
-    Represents one round (or turn) of the game.
-
-    Attributes:
-        your_shape: The opponent's chosen shape for this round.
-        my_shape: My chosen shape for this round.
-    """
     your_shape: Shape
     my_shape: Shape
 
@@ -112,48 +101,22 @@ class Turn:
         return self.your_score(), self.my_score()
 
 
-@dataclass
-class Solver:
-    """
-    The main code to solve the puzzle and display the results.
-    """
-    def __init__(self):
-        self.data = None
+class Solver(AbstractSolver):
+    def __init__(self) -> None:
+        super().__init__()
 
-    @staticmethod
-    def init_data() -> list[Turn]:
-        """
-        Read the puzzle data.
-
-        Returns:
-            A list of Turns.
-        """
-        turns = []
-        with open('../resources/day02.data', 'r') as data_file:
-            lines = data_file.read().splitlines()
-        for line in lines:
+    def init_data(self) -> list[Any]:
+        data = []
+        day = os.path.basename(__file__)[:-3]
+        for line in self.get_data(day):
             (your_choice, my_choice) = tuple(line.split())
-            turns.append(Turn(your_shape=Shape(your_choice),
-                              my_shape=Shape(my_choice)))
+            data.append(Turn(your_shape=Shape(your_choice),
+                             my_shape=Shape(my_choice)))
 
-        return turns
+        return data
 
     @staticmethod
     def decode_shapes(data: list[Turn]) -> list[Turn]:
-        """
-        Create a new list of Turns assuming that the encoded strategy guide
-        provides shapes to play on each turn.
-
-        X means rock, Y means paper, and Z means scissors.
-
-        Parameters:
-            data: The strategy guide that specifies which shape I should
-            play in each round.
-
-        Returns:
-            A new list of Turns with the shape I should play substituted for
-            the encoded values in the strategy guide.
-        """
         turns = []
         for turn in data:
             new_my_shape = Shape(chr(ord(turn.my_shape.value) - 23))
@@ -162,20 +125,6 @@ class Solver:
 
     @staticmethod
     def decode_outcomes(data: list[Turn]) -> list[Turn]:
-        """
-        Create a new list of Turns assuming that the encoded strategy guide
-        provides the desired outcome for each turn.
-
-        X means I should lose, Y means draw, and Z means I should win.
-
-        Parameters:
-            data: The strategy guide that specifies which shape I should
-            play in each round.
-
-        Returns:
-            A new list of Turns with the shape I should play to achieve
-            the outcome specified by the encoded values in the strategy guide.
-        """
         turns = []
         for turn in data:
             if turn.i_should_win():
@@ -191,15 +140,6 @@ class Solver:
 
     @staticmethod
     def play(turns: list[Turn]) -> int:
-        """
-        Play the game according to specified list of Turns.
-
-        Evaluate each turn. Determine the winning player and the score
-        for both players.
-
-        Parameters:
-            turns: The list Turns to evaluate.
-        """
         your_total = 0
         my_total = 0
         for turn in turns:
@@ -208,66 +148,11 @@ class Solver:
             my_total += score[1]
         return my_total
 
-    @staticmethod
-    def print_info(part: str, init_timer: Timer,
-                   solver_timer: Timer, answer: int) -> None:
-        """
-        Print the answer and elapsed time information for this puzzle.
+    def solve_part_1(self, data: list[Any]) -> int:
+        return self.play(self.decode_shapes(data))
 
-        Parameters:
-            part: Description of puzzle. 'Part 1' or 'Part 2'.
-            init_timer: Elapsed time for initialization.
-            solver_timer: Elapsed time to solve the puzzle.
-            answer: The puzzle answer.
-        """
-        print(f'{part}\n'
-              f'\tElapsed Time\n'
-              f'\t\t  Init: {init_timer.elapsed_time()}\n'
-              f'\t\t   Run: {solver_timer.elapsed_time()}\n'
-              f'\t\tAnswer: {answer}')
-
-    def part_1(self) -> None:
-        """
-        Play the game assuming X means rock, Y means paper, and Z means
-        scissors.
-
-        Read the puzzle data, play the game, and print the results.
-        """
-        init_timer = Timer()
-        data = self.init_data()
-        init_timer.stop()
-
-        solver_timer = Timer()
-        answer = self.play(self.decode_shapes(data))
-        solver_timer.stop()
-
-        self.print_info(part='Part 1', init_timer=init_timer,
-                        solver_timer=solver_timer, answer=answer)
-
-    def part_2(self) -> None:
-        """
-        Play the game assuming X means I should lose a round, Y means the
-        round should be a draw, and Z I should win a round.
-        scissors.
-
-        Read the puzzle data, play the game, and print the results.
-        """
-        init_timer = Timer()
-        data = self.init_data()
-        init_timer.stop()
-
-        solver_timer = Timer()
-        answer = self.play(self.decode_outcomes(data))
-        solver_timer.stop()
-        self.print_info(part='Part 2', init_timer=init_timer,
-                        solver_timer=solver_timer, answer=answer)
-
-    def run(self) -> None:
-        """
-        Run the puzzle solver.
-        """
-        self.part_1()
-        self.part_2()
+    def solve_part_2(self, data: list[Any]) -> int:
+        return self.play(self.decode_outcomes(data))
 
 
 def main() -> None:

0bcab7533a8f1415b6f19bbee15fec4a3d98ab41
Chris Britton (RD-US) <chris_britton@trendmicro.com>
2022-12-04 01:17:06 -0600

    Cleaned up day 4. Refactored Solver class.

--- src/main/python/04.py
+++ src/main/python/04.py
@@ -9,97 +9,66 @@ import re
 from dataclasses import dataclass
 from typing import Any
 
-from src.main.python.util import Timer, print_info
+from src.main.python.util import AbstractSolver
 
 
 @dataclass
-class SectionRange:
+class Sections:
     low: int
     high: int
 
-    def contains(self, section_range: 'SectionRange') -> bool:
+    def contains(self, section_range: 'Sections') -> bool:
         return self.low <= section_range.low <= self.high and \
             self.low <= section_range.high <= self.high
 
-    def overlaps(self, section_range: 'SectionRange') -> bool:
+    def overlaps(self, section_range: 'Sections') -> bool:
         return self.low <= section_range.low <= self.high or \
             self.low <= section_range.high <= self.high
 
 
 @dataclass
 class Pair:
-    section_range_1: SectionRange
-    section_range_2: SectionRange
+    sections_1: Sections
+    sections_2: Sections
 
-    def is_redundant(self):
-        return self.section_range_1.contains(self.section_range_2) or \
-            self.section_range_2.contains(self.section_range_1)
+    def has_redundant(self) -> bool:
+        return self.sections_1.contains(self.sections_2) or \
+            self.sections_2.contains(self.sections_1)
 
-    def is_overlap(self):
-        return self.section_range_1.overlaps(self.section_range_2) or \
-            self.section_range_2.overlaps(self.section_range_1)
+    def has_overlap(self) -> bool:
+        return self.sections_1.overlaps(self.sections_2) or \
+            self.sections_2.overlaps(self.sections_1)
 
 
-@dataclass
-class Solver:
+class Solver(AbstractSolver):
     def __init__(self) -> None:
-        self.data = None
+        super().__init__()
 
-    @staticmethod
-    def init_data() -> tuple[list[Any], Timer]:
-        timer = Timer()
-
+    def init_data(self) -> list[Any]:
+        pattern = r'([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)'
         data = []
         day = os.path.basename(__file__)[:-3]
-        with open(f'../resources/day{day}.data', 'r') as data_file:
-            lines = data_file.read().splitlines()
-        pattern = r'([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)'
-        for line in lines:
+        for line in self.get_data(day):
             m = re.search(pattern, line)
             min_1 = int(m.group(1))
             max_1 = int(m.group(2))
             min_2 = int(m.group(3))
             max_2 = int(m.group(4))
-            data.append(
-                Pair(SectionRange(min_1, max_1), SectionRange(min_2, max_2)))
+            data.append(Pair(Sections(min_1, max_1), Sections(min_2, max_2)))
 
-        timer.stop()
+        return data
 
-        return data, timer
-
-    @staticmethod
-    def solve_part_1(data: list[Any]) -> tuple[int, Timer]:
-        timer = Timer()
+    def solve_part_1(self, data: list[Pair]) -> int:
         answer = 0
         for pair in data:
-            answer += 1 if pair.is_redundant() else 0
-        timer.stop()
-        return answer, timer
+            answer += 1 if pair.has_redundant() else 0
+        return answer
 
-    def part_1(self) -> None:
-        data, init_timer = self.init_data()
-        answer, solver_timer = self.solve_part_1(data)
-        print_info(part='Part 1', init_timer=init_timer,
-                   solver_timer=solver_timer, answer=answer)
-
-    @staticmethod
-    def solve_part_2(data: list[Any]) -> tuple[int, Timer]:
-        timer = Timer()
+    def solve_part_2(self, data: list[Pair]) -> int:
         answer = 0
         for pair in data:
-            answer += 1 if pair.is_overlap() else 0
-        timer.stop()
-        return answer, timer
-
-    def part_2(self) -> None:
-        data, init_timer = self.init_data()
-        answer, solver_timer = self.solve_part_2(data)
-        print_info(part='Part 2', init_timer=init_timer,
-                   solver_timer=solver_timer, answer=answer)
-
-    def run(self) -> None:
-        self.part_1()
-        self.part_2()
+            answer += 1 if pair.has_overlap() else 0
+        return answer
 
 
 def main() -> None:
--- src/main/python/util.py
+++ src/main/python/util.py
@@ -1,4 +1,6 @@
 import time
+from abc import abstractmethod, ABC
+from typing import Any
 
 
 class Timer:
@@ -46,19 +48,60 @@ class Timer:
         return f'{t:.2f} {unit}'
 
 
-def print_info(part: str, init_timer: Timer,
-               solver_timer: Timer, answer: int) -> None:
-    """
-    Print the answer and elapsed time information for this puzzle.
+class AbstractSolver(ABC):
+    def __init__(self) -> None:
+        self.data = None
 
-    Parameters:
-        part: Description of puzzle. 'Part 1' or 'Part 2'.
-        init_timer: Elapsed time for initialization.
-        solver_timer: Elapsed time to solve the puzzle.
-        answer: The puzzle answer.
-    """
-    print(f'{part}\n'
-          f'\tElapsed Time\n'
-          f'\t\t  Init: {init_timer.elapsed_time()}\n'
-          f'\t\t   Run: {solver_timer.elapsed_time()}\n'
-          f'\t\tAnswer: {answer}')
+    @abstractmethod
+    def init_data(self) -> list[Any]:
+        pass
+
+    @abstractmethod
+    def solve_part_1(self, data: list[Any]) -> int:
+        pass
+
+    @abstractmethod
+    def solve_part_2(self, data: list[Any]) -> int:
+        pass
+
+    def part_1(self) -> None:
+        init_timer = Timer()
+        data = self.init_data()
+        init_timer.stop()
+
+        solver_timer = Timer()
+        answer = self.solve_part_1(data)
+        solver_timer.stop()
+
+        self.print_info(part='Part 1', init_timer=init_timer,
+                        solver_timer=solver_timer, answer=answer)
+
+    def part_2(self) -> None:
+        init_timer = Timer()
+        data = self.init_data()
+        init_timer.stop()
+
+        solver_timer = Timer()
+        answer = self.solve_part_2(data)
+        solver_timer.stop()
+
+        self.print_info(part='Part 2', init_timer=init_timer,
+                        solver_timer=solver_timer, answer=answer)
+
+    def run(self) -> None:
+        self.part_1()
+        self.part_2()
+
+    @staticmethod
+    def print_info(part: str, init_timer: Timer,
+                   solver_timer: Timer, answer: int) -> None:
+        print(f'{part}\n'
+              f'\tElapsed Time\n'
+              f'\t\t  Init: {init_timer.elapsed_time()}\n'
+              f'\t\t   Run: {solver_timer.elapsed_time()}\n'
+              f'\t\tAnswer: {answer}')
+
+    @staticmethod
+    def get_data(day: str) -> list[str]:
+        with open(f'../resources/day{day}.data', 'r') as data_file:
+            return data_file.read().splitlines()

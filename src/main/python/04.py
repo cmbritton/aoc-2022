8703215a8309371977f15b4cfd88055dda172bfb
Chris Britton (RD-US) <chris_britton@trendmicro.com>
2022-12-03 23:59:34 -0600

    Day 4 works (initial)

--- src/main/python/04.py
+++ src/main/python/04.py
@@ -1,23 +1,43 @@
 #!/usr/bin/env python3
 """
-Day x:
+Day 4: Camp Cleanup
 
-https://adventofcode.com/2022/day/x
+https://adventofcode.com/2022/day/4
 """
 import os.path
+import re
 from dataclasses import dataclass
-from functools import reduce
 from typing import Any
 
 from src.main.python.util import Timer, print_info
 
 
 @dataclass
-class SomeData:
-    value: str
+class SectionRange:
+    low: int
+    high: int
 
-    def foo(self) -> int:
-        return 5
+    def contains(self, section_range: 'SectionRange') -> bool:
+        return self.low <= section_range.low <= self.high and \
+            self.low <= section_range.high <= self.high
+
+    def overlaps(self, section_range: 'SectionRange') -> bool:
+        return self.low <= section_range.low <= self.high or \
+            self.low <= section_range.high <= self.high
+
+
+@dataclass
+class Pair:
+    section_range_1: SectionRange
+    section_range_2: SectionRange
+
+    def is_redundant(self):
+        return self.section_range_1.contains(self.section_range_2) or \
+            self.section_range_2.contains(self.section_range_1)
+
+    def is_overlap(self):
+        return self.section_range_1.overlaps(self.section_range_2) or \
+            self.section_range_2.overlaps(self.section_range_1)
 
 
 @dataclass
@@ -33,8 +53,15 @@ class Solver:
         day = os.path.basename(__file__)[:-3]
         with open(f'../resources/day{day}.data', 'r') as data_file:
             lines = data_file.read().splitlines()
+        pattern = r'([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)'
         for line in lines:
-            data.append(SomeData(line.strip()))
+            m = re.search(pattern, line)
+            min_1 = int(m.group(1))
+            max_1 = int(m.group(2))
+            min_2 = int(m.group(3))
+            max_2 = int(m.group(4))
+            data.append(
+                Pair(SectionRange(min_1, max_1), SectionRange(min_2, max_2)))
 
         timer.stop()
 
@@ -43,7 +70,9 @@ class Solver:
     @staticmethod
     def solve_part_1(data: list[Any]) -> tuple[int, Timer]:
         timer = Timer()
-        answer = reduce(lambda x, y: x + y, [r.foo() for r in data])
+        answer = 0
+        for pair in data:
+            answer += 1 if pair.is_redundant() else 0
         timer.stop()
         return answer, timer
 
@@ -56,7 +85,9 @@ class Solver:
     @staticmethod
     def solve_part_2(data: list[Any]) -> tuple[int, Timer]:
         timer = Timer()
-        answer = reduce(lambda x, y: x + y, [r.foo() for r in data])
+        answer = 0
+        for pair in data:
+            answer += 1 if pair.is_overlap() else 0
         timer.stop()
         return answer, timer
 

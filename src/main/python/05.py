5a7c5b36482b1ceadc704414d2273245e6a7b1d7
Chris Britton (RD-US) <chris_britton@trendmicro.com>
2022-12-05 01:08:39 -0600

    Day 5 part 1 works

--- src/main/python/05.py
+++ src/main/python/05.py
@@ -6,6 +6,7 @@ https://adventofcode.com/2022/day/5
 """
 import os.path
 import re
+from collections import defaultdict
 from dataclasses import dataclass
 from typing import Any
 
@@ -13,34 +14,95 @@ from src.main.python.util import AbstractSolver
 
 
 @dataclass
-class MyData:
-    value: str
+class Stack:
+    value: list[str]
+
+
+class Cargo:
+    stacks: defaultdict[int, list]
+
+    def __init__(self) -> None:
+        self.stacks = defaultdict(list)
+
+    def add_crate(self, stack, crate):
+        self.stacks[stack].append(crate)
+
+    def move_crate(self, src_stack, dst_stack):
+        self.add_crate(dst_stack, self.stacks[src_stack].pop())
+
+    def move_crates(self, qty, src_stack, dst_stack):
+        for i in range(qty):
+            self.move_crate(src_stack, dst_stack)
+
+    def get_top_crates(self):
+        top_creates = []
+        for i in range(len(self.stacks)):
+            if self.stacks[i]:
+                top_creates.append(self.stacks[i][len(self.stacks[i]) - 1])
+
+        return ''.join(top_creates)
+
+
+@dataclass
+class Move:
+    qty: int
+    src_stack: int
+    dst_stack: int
 
 
 class Solver(AbstractSolver):
+    cargo: Cargo
+
     def __init__(self) -> None:
         super().__init__()
 
     def init_data(self) -> list[Any]:
-        pattern = r'(.*)'
-        data = []
         day = os.path.basename(__file__)[:-3]
-        for line in self.get_data(day):
-            m = re.search(pattern, line)
-            data.append(MyData(m.group(1)))
+        data = self.get_data(day)
 
-        return data
+        idx = data.index('')
+        stack_data = data[0:idx]
+        stack_data.reverse()
+        stack_header_data = stack_data[0]
+        stack_data = stack_data[1:]
+        stack_names = stack_header_data.split()
 
-    def solve_part_1(self, data: list[Any]) -> int:
+        move_data = data[idx + 1:]
+
+        stack_count = len(stack_names)
+        # stack_header_pattern = r'^.(.)...(.)...(.).$'
+        stack_header_pattern = r'^.(.)...(.)...(.)...(.)...(.)...(.)...(' \
+                               r'.)...(.)...(.).$'
+
+        self.cargo = Cargo()
+        for line in stack_data:
+            m = re.search(stack_header_pattern, line)
+            # crates = m.group(1, 2, 3)
+            crates = m.group(1, 2, 3, 4, 5, 6, 7, 8, 9)
+            for i, crate in enumerate(crates):
+                if crate.strip():
+                    self.cargo.add_crate(i, crate)
+
+        moves = []
+        move_pattern = r'^move (\d*) from (\d*) to (\d*)$'
+        for line in move_data:
+            m = re.search(move_pattern, line)
+            quantity, src_stack, dst_stack = m.group(1, 2, 3)
+            moves.append(Move(int(quantity), 
+                              int(src_stack) - 1, int(dst_stack) - 1))
+
+        return moves
+
+    def solve_part_1(self, data: list[Any]) -> Any:
         answer = 0
-        for d in data:
-            answer += 1 if len(d) > 0 else 0
-        return answer
+        for move in data:
+            self.cargo.move_crates(move.qty, move.src_stack, move.dst_stack)
+        return self.cargo.get_top_crates()
 
-    def solve_part_2(self, data: list[Any]) -> int:
+    def solve_part_2(self, data: list[Any]) -> Any:
         answer = 0
-        for d in data:
-            answer += 1 if len(d) > 5 else 0
+        # for d in data:
+        #     answer += 1 if len(d) > 5 else 0
         return answer
 
 

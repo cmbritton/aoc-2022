c8472f6ce21a1c1504b778dafa93d2459d2b56ff
Chris Britton (RD-US) <chris_britton@trendmicro.com>
2022-12-05 20:32:12 -0600

    Prepare for day 6

---
+++ src/main/python/06.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Day 6: Something
+
+https://adventofcode.com/2022/day/6
+"""
+import os.path
+import re
+from dataclasses import dataclass
+from typing import Any
+
+from src.main.python.util import AbstractSolver
+
+
+@dataclass
+class MyData:
+    value: str
+
+
+class Solver(AbstractSolver):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def init_data(self) -> list[Any]:
+        pattern = r'(.*)'
+        data = []
+        day = os.path.basename(__file__)[:-3]
+        for line in self.get_data(day):
+            m = re.search(pattern, line)
+            data.append(MyData(m.group(1)))
+
+        return data
+
+    def solve_part_1(self, data: list[Any]) -> int:
+        answer = 0
+        for d in data:
+            answer += 1 if len(d) > 0 else 0
+        return answer
+
+    def solve_part_2(self, data: list[Any]) -> int:
+        answer = 0
+        for d in data:
+            answer += 1 if len(d) > 5 else 0
+        return answer
+
+
+def main() -> None:
+    solver = Solver()
+    solver.run()
+
+
+if __name__ == "__main__":
+    main()

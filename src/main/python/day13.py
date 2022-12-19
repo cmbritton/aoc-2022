5cce4cea38ef254e48dafe32e5b0acb087bb4ca3
Chris Britton (RD-US) <chris_britton@trendmicro.com>
2022-12-19 15:59:08 -0600

    Day 13 part 2 works

--- src/main/python/day13.py
+++ src/main/python/day13.py
@@ -5,7 +5,9 @@ Day 13: Distress Signal
 https://adventofcode.com/2022/day/13
 """
 import os.path
+import re
 from dataclasses import dataclass
+from functools import cmp_to_key
 from itertools import zip_longest
 from typing import Any
 
@@ -39,11 +41,11 @@ class Solver(AbstractSolver):
 
     def compare_int(self, p1, p2):
         if p1 < p2:
-            return 'valid'
+            return -1
         elif p1 > p2:
-            return 'notvalid'
+            return 1
         else:
-            return 'undetermined'
+            return 0
 
     def compare(self, p1, p2):
         if isinstance(p1, int) and (isinstance(p2, int)):
@@ -55,29 +57,77 @@ class Solver(AbstractSolver):
         else:
             return self.is_valid(p1, p2)
 
+    def compare_packets(self, p1, p2):
+        if isinstance(p1, int) and (isinstance(p2, int)):
+            if p1 < p2:
+                return -1
+            elif p1 > p2:
+                return 1
+            else:
+                return 0
+        elif isinstance(p1, int) and (isinstance(p2, list)):
+            return self.is_valid([p1], p2)
+        elif isinstance(p1, list) and (isinstance(p2, int)):
+            return self.is_valid(p1, [p2])
+        else:
+            return self.is_valid(p1, p2)
+
     def is_valid(self, p1, p2):
         for v1, v2 in zip_longest(p1, p2):
             if v1 is None:
-                return 'valid'
+                return -1
             elif v2 is None:
-                return 'notvalid'
+                return 1
 
             result = self.compare(v1, v2)
-            if result == 'valid':
-                return 'valid'
-            elif result == 'notvalid':
-                return 'notvalid'
-        return 'undetermined'
+            if result == -1:
+                return -1
+            elif result == 1:
+                return 1
+        return 0
 
     def solve_part_1(self, data: Any) -> int:
         indexes = []
         for i, packet_pair in enumerate(data):
-            if self.is_valid(packet_pair[0], packet_pair[1]) == 'valid':
+            if self.is_valid(packet_pair[0], packet_pair[1]) == -1:
                 indexes.append(i + 1)
         return sum(indexes)
 
     def solve_part_2(self, data: Any) -> int:
-        return 0
+        packets = []
+        for p1, p2 in data:
+            packets.append(p1)
+            packets.append(p2)
+
+        packets.append([[2]])
+        packets.append([[6]])
+
+        packet_map = dict()
+        # pattern = r'\[\]'
+        for p in packets:
+            # m = re.search(pattern, str(p))
+            # if m:
+            #     key = str(p).replace('[', '').replace(']', '')
+            # else:
+            #     key = str(p).replace('[', ')').replace(']', '(')
+            s = str(p)
+            if '[]' in s:
+                key = str(p).replace('[', ')').replace(']', '(')
+            else:
+                key = str(p).replace('[', '').replace(']', '')
+            packet_map[key] = p
+
+        sorted_packets = []
+        for p in sorted(packets, key=cmp_to_key(self.is_valid)):
+        # for k in sorted(packet_map.keys()):
+            sorted_packets.append(p)
+
+# 30800 is too high
+
+        x1 = sorted_packets.index([[2]]) + 1
+        x2 = sorted_packets.index([[6]]) + 1
+
+        return x1 * x2
 
     def get_day(self) -> str:
         return os.path.basename(__file__)[3:5]

6fd32ba14ca5e3c3130f0d4099e3cef1bed84daf
Chris Britton (RD-US) <chris_britton@trendmicro.com>
2022-12-22 02:52:19 -0600

    Day 14 part 1 works

--- src/main/python/day14.py
+++ src/main/python/day14.py
@@ -5,34 +5,203 @@ Day 14: Regolith Reservoir
 https://adventofcode.com/2022/day/14
 """
 import os.path
-import re
-from dataclasses import dataclass
+from collections import namedtuple
+from itertools import pairwise
 from typing import Any
 
 from src.main.python.util import AbstractSolver
 
-
-@dataclass
-class MyData:
-    value: str
+Point = namedtuple('Point', 'x y')
+Line = namedtuple('Line', 'p1 p2')
 
 
 class Solver(AbstractSolver):
     def __init__(self) -> None:
         super().__init__()
+        self.rock_veins = None
+        self.sand_points = []
+        self.count = 0
+        self.sand_history = []
 
     def init_data(self, data_file_path: str = None) -> Any:
         data = self.get_data(self.get_day(), data_file_path)
-        pattern = r'(.*)'
-        my_data = []
+        rock_veins = []
         for line in data:
-            m = re.search(pattern, line)
-            my_data.append(MyData(m.group(1)))
+            points = line.split(' -> ')
+            for p1, p2 in pairwise(points):
+                p1x, p1y = p1.split(',')
+                p2x, p2y = p2.split(',')
+                rock_veins.append(
+                    Line(Point(int(p1x), int(p1y)), Point(int(p2x), int(p2y))))
 
-        return my_data
+        min_x = 500
+        max_x = 500
+        min_y = 0
+        max_y = 0
+        
+        for line in rock_veins:
+            min_x = min(min_x, line.p1.x)
+            min_x = min(min_x, line.p2.x)
+            max_x = max(max_x, line.p1.x)
+            max_x = max(max_x, line.p2.x)
+            
+            min_y = min(min_y, line.p1.y)
+            min_y = min(min_y, line.p2.y)
+            max_y = max(max_y, line.p1.y)
+            max_y = max(max_y, line.p2.y)
+
+        print(f'({min_x}, {min_y}) ({max_x}, {max_y})')
+        return rock_veins
+
+    def is_occupied_by_sand(self, point):
+        return point in self.sand_points
+
+    def is_occupied_by_rock(self, point):
+        for line in self.rock_veins:
+            if point.x == line.p1.x == line.p2.x:
+                y1 = min(line.p1.y, line.p2.y)
+                y2 = max(line.p1.y, line.p2.y)
+                if y1 <= point.y <= y2:
+                    return True
+            elif point.y == line.p1.y == line.p2.y:
+                x1 = min(line.p1.x, line.p2.x)
+                x2 = max(line.p1.x, line.p2.x)
+                if x1 <= point.x <= x2:
+                    return True
+
+        return False
+
+    def is_occupied(self, point):
+        if self.is_occupied_by_sand(point):
+            return True
+        return self.is_occupied_by_rock(point)
+
+    def can_drop_left(self, point):
+        return not self.is_occupied(Point(point.x - 1, point.y + 1))
+
+    def can_drop_right(self, point):
+        return not self.is_occupied(Point(point.x + 1, point.y + 1))
+
+    def can_drop_down(self, point):
+        return not self.is_occupied(Point(point.x, point.y + 1))
+
+    def drop(self, point, indent=False):
+        # Return True when done
+        indentstr = '    ' if indent else ''
+        # indentstr = '    ' * indent
+        # print(f'{indentstr}{point}')
+        self.sand_history.append(f'{indentstr}{point}')
+        if self.can_drop_down(point):
+            obstruction = self.next_obstruction(point)
+            if obstruction is None:
+                return True
+            p = Point(point.x, obstruction.y - 1)
+            # print(f'\t{indentstr}drops down to {p}')
+            self.sand_history.append(f'\t{indentstr}drops down to {p}')
+            result = self.drop(p, indent + 1)
+            return result
+        elif self.can_drop_left(point):
+            p = Point(point.x - 1, point.y + 1)
+            # print(f'\t{indentstr}rolls left to {p}')
+            self.sand_history.append(f'\t{indentstr}rolls left to {p}')
+            result = self.drop(p, indent + 1)
+            return result
+        elif self.can_drop_right(point):
+            p = Point(point.x + 1, point.y + 1)
+            # print(f'\t{indentstr}rolls right to {p}')
+            self.sand_history.append(f'\t{indentstr}rolls right to {p}')
+            result = self.drop(p, indent + 1)
+            return result
+        else:
+            # print(f'\t{indentstr}comes to rest at {point}')
+            self.sand_history.append(f'\t{indentstr}comes to rest at {point}')
+            if point in self.sand_points:
+                self.print()
+                raise RuntimeError(f'Duplicate resting point {point}')
+            else:
+                self.sand_points.append(point)
+            return False
+
+    def next_obstruction(self, point):
+        ys = []
+
+        for p in self.sand_points:
+            if p.x == point.x:
+                ys.append(p.y)
+
+        for line in self.rock_veins:
+            x1 = min(line.p1.x, line.p2.x)
+            x2 = max(line.p1.x, line.p2.x)
+            if x1 <= point.x <= x2:
+                ys.append(line.p1.y)
+                ys.append(line.p2.y)
+
+        filtered_ys = list(filter(lambda x: x > point.y, ys))
+        if len(filtered_ys) == 0:
+            return None
+        y = min(filtered_ys)
+        # print(f'Next obstruction for {point}: ', end='')
+        if y == point.y:
+            # print('None')
+            return None
+        else:
+            p = Point(point.x, y)
+            # print(f'{p}')
+            return p
+
+    def print(self, point=None):
+        if True:
+            return
+        indentstr = ''
+        if point is not None:
+            indentstr += '    '
+        for y in range(162):
+            print(indentstr, end='')
+            for x in range(480, 538):
+                p = Point(x, y)
+                if p == point:
+                    print('O', end='')
+                elif self.is_occupied_by_sand(p) and self.is_occupied_by_rock(p):
+                    print('X', end='')
+                elif self.is_occupied_by_sand(p):
+                    if len(self.sand_points) > 1:
+                        try:
+                            idx = self.sand_points[-26:].index(p)
+                            print(chr(65 + idx), end='')
+                        except ValueError:
+                            print('o', end='')
+                        # if p == self.sand_points[-1]:
+                        #    print('Z', end='')
+                        # else:
+                        #     print('o', end='')
+                    else:
+                        print('o', end='')
+                elif self.is_occupied_by_rock(p):
+                    print('#', end='')
+                else:
+                    if x == 500:
+                        print('+', end='')
+                    else:
+                        print('.', end='')
+            print('')
 
     def solve_part_1(self, data: Any) -> int:
-        return 0
+        self.rock_veins = data
+        done = False
+        sand_point = Point(500, 0)
+        # self.print()
+        while not done:
+            self.count += 1
+            # print(f'\nnew sand {self.count}')
+            prev = len(self.sand_points)
+            self.sand_history.clear()
+            done = self.drop(sand_point)
+            # self.print(self)
+            if len(self.sand_points) == prev:
+                print(f'Sand not added in iteration {self.count}')
+                print(self.sand_history)
+                print(self)
+        return len(self.sand_points)
 
     def solve_part_2(self, data: Any) -> int:
         return 0

f5f5bc1cc6d8a8b7b8a94d17fa80d9087f2f79ab
Chris Britton (RD-US) <chris_britton@trendmicro.com>
2022-12-22 19:18:35 -0600

    Day 14 refactor for performance

--- README.md
+++ README.md
@@ -81,7 +81,7 @@ No answers here! Just elapsed times for each puzzle solution.
 |[Day 11: Monkey in the Middle](https://adventofcode.com/2022/11)|6.00 milliseconds|2.99 seconds|
 |[Day 12: Hill Climbing Algorithm](https://adventofcode.com/2022/12)|620.97 milliseconds|99.38 seconds|
 |[Day 13: Distress Signal](https://adventofcode.com/2022/13)|9.42 milliseconds|10.92 milliseconds|
-|[Day 14: Regolith Reservoir](https://adventofcode.com/2022/13)|6.02 seconds|896.42 seconds|
+|[Day 14: Regolith Reservoir](https://adventofcode.com/2022/13)|56.52 milliseconds|3.17 seconds|
 |Day 15: Unavailable|Unsolved|Unsolved|
 |Day 16: Unavailable|Unsolved|Unsolved|
 |Day 17: Unavailable|Unsolved|Unsolved|
--- src/main/python/day14.py
+++ src/main/python/day14.py
@@ -11,6 +11,8 @@ from typing import Any
 
 from src.main.python.util import AbstractSolver
 
+INFINITY = 999999
+
 Point = namedtuple('Point', 'x y')
 Line = namedtuple('Line', 'p1 p2')
 
@@ -18,149 +20,122 @@ Line = namedtuple('Line', 'p1 p2')
 class Solver(AbstractSolver):
     def __init__(self) -> None:
         super().__init__()
-        self.rock_veins = None
-        self.sand_points = []
-        self.count = 0
-        self.sand_history = []
-        self.floor = False
-        self.max_y = 0
+        self.matrix = []
+
+    @staticmethod
+    def is_vertical(line: Line) -> bool:
+        return line.p1.x == line.p2.x
+
+    def line_to_points(self, line: Line) -> list[Point]:
+        if self.is_vertical(line):
+            min_y = min(line.p1.y, line.p2.y)
+            max_y = max(line.p1.y, line.p2.y)
+            return [Point(line.p1.x, y) for y in
+                    range(min_y, max_y + 1)]
+        else:
+            min_x = min(line.p1.x, line.p2.x)
+            max_x = max(line.p1.x, line.p2.x)
+            return [Point(x, line.p1.y) for x in
+                    range(min_x, max_x + 1)]
 
     def init_data(self, data_file_path: str = None) -> Any:
         data = self.get_data(self.get_day(), data_file_path)
-        rock_veins = []
+        max_x = -INFINITY
+        max_y = -INFINITY
+        rock_lines = []
         for line in data:
             points = line.split(' -> ')
             for p1, p2 in pairwise(points):
                 p1x, p1y = p1.split(',')
                 p2x, p2y = p2.split(',')
-                rock_veins.append(
-                        Line(Point(int(p1x), int(p1y)),
-                             Point(int(p2x), int(p2y))))
+                max_x = max(max_x, int(p1x), int(p2x))
+                max_y = max(max_y, int(p1y), int(p2y))
+                rock_lines.append(Line(Point(int(p1x), int(p1y)),
+                                       Point(int(p2x), int(p2y))))
 
-        min_x = 500
-        max_x = 500
-        min_y = 0
-        max_y = 0
+        matrix = [['.'] * (max_x * 2) for _ in range(max_y + 1)]
+        for line in rock_lines:
+            for p in self.line_to_points(line):
+                matrix[p.y][p.x] = '#'
 
-        for line in rock_veins:
-            min_x = min(min_x, line.p1.x)
-            min_x = min(min_x, line.p2.x)
-            max_x = max(max_x, line.p1.x)
-            max_x = max(max_x, line.p2.x)
-
-            min_y = min(min_y, line.p1.y)
-            min_y = min(min_y, line.p2.y)
-            max_y = max(max_y, line.p1.y)
-            max_y = max(max_y, line.p2.y)
-
-        self.max_y = max_y
-        return rock_veins
-
-    def is_occupied_by_sand(self, point):
-        return point in self.sand_points
-
-    def is_occupied_by_rock(self, point):
-        if self.floor:
-            if point.y == self.max_y + 2:
-                return True
-        for line in self.rock_veins:
-            if point.x == line.p1.x == line.p2.x:
-                y1 = min(line.p1.y, line.p2.y)
-                y2 = max(line.p1.y, line.p2.y)
-                if y1 <= point.y <= y2:
-                    return True
-            elif point.y == line.p1.y == line.p2.y:
-                x1 = min(line.p1.x, line.p2.x)
-                x2 = max(line.p1.x, line.p2.x)
-                if x1 <= point.x <= x2:
-                    return True
-
-        return False
+        return matrix
 
     def is_occupied(self, point):
-        if self.is_occupied_by_sand(point):
-            return True
-        return self.is_occupied_by_rock(point)
+        return self.matrix[point.y][point.x] != '.'
+
+    def is_point_in_bounds(self, point: Point):
+        return len(self.matrix[0]) > point.x >= 0 and \
+            len(self.matrix) > point.y >= 0
 
     def can_drop_left(self, point):
-        return not self.is_occupied(Point(point.x - 1, point.y + 1))
+        p = Point(point.x - 1, point.y + 1)
+        return self.is_point_in_bounds(p) and \
+            not self.is_occupied(p)
 
     def can_drop_right(self, point):
-        return not self.is_occupied(Point(point.x + 1, point.y + 1))
+        p = Point(point.x + 1, point.y + 1)
+        return self.is_point_in_bounds(p) and \
+            not self.is_occupied(p)
 
     def can_drop_down(self, point):
-        return not self.is_occupied(Point(point.x, point.y + 1))
+        p = Point(point.x, point.y + 1)
+        return self.is_point_in_bounds(p) and \
+            not self.is_occupied(p)
 
-    def drop(self, point, indent=False):
+    def drop(self, point):
         # Return True when done
         if self.can_drop_down(point):
             obstruction = self.next_obstruction(point)
             if obstruction is None:
                 return True
-            result = self.drop(Point(point.x, obstruction.y - 1), True)
-            return result
+            return self.drop(Point(point.x, obstruction.y - 1))
         elif self.can_drop_left(point):
-            result = self.drop(Point(point.x - 1, point.y + 1), True)
-            return result
+            return self.drop(Point(point.x - 1, point.y + 1))
         elif self.can_drop_right(point):
-            result = self.drop(Point(point.x + 1, point.y + 1), True)
-            return result
+            return self.drop(Point(point.x + 1, point.y + 1))
         else:
-            if point in self.sand_points:
-                raise RuntimeError(f'Duplicate resting point {point}')
-            else:
-                self.sand_points.append(point)
-            if self.floor and point == (500, 0):
-                return True
-            else:
-                return False
+            return self.add_sand_at_rest(point)
+
+    def add_sand_at_rest(self, point):
+        # Return True when done
+        if point.y == len(self.matrix) - 1:
+            return True
+        self.matrix[point.y][point.x] = 'o'
+        if point == (500, 0):
+            return True
+        else:
+            return False
 
     def next_obstruction(self, point):
-        ys = []
+        for y in range(point.y, len(self.matrix)):
+            if self.matrix[y][point.x] != '.':
+                return Point(point.x, y)
+        return None
 
-        for p in self.sand_points:
-            if p.x == point.x:
-                ys.append(p.y)
-
-        for line in self.rock_veins:
-            x1 = min(line.p1.x, line.p2.x)
-            x2 = max(line.p1.x, line.p2.x)
-            if x1 <= point.x <= x2:
-                ys.append(line.p1.y)
-                ys.append(line.p2.y)
-
-        if self.floor:
-            ys.append(self.max_y + 2)
-        filtered_ys = list(filter(lambda x: x > point.y, ys))
-        if len(filtered_ys) == 0:
-            return None
-        y = min(filtered_ys)
-        if y == point.y:
-            return None
-        else:
-            return Point(point.x, y)
+    def sand_count(self):
+        count = 0
+        for y in range(len(self.matrix)):
+            for x in range(len(self.matrix[y])):
+                count += 1 if self.matrix[y][x] == 'o' else 0
+        return count
 
     def solve_part_1(self, data: Any) -> int:
-        self.rock_veins = data
+        self.matrix = data
         done = False
         sand_point = Point(500, 0)
         while not done:
-            self.count += 1
-            self.sand_history.clear()
             done = self.drop(sand_point)
-        return len(self.sand_points)
+        return self.sand_count()
 
     def solve_part_2(self, data: Any) -> int:
-        self.rock_veins = data
-        self.floor = True
+        self.matrix = data
+        self.matrix.append(['.'] * len(self.matrix[-1]))
+        self.matrix.append(['#'] * len(self.matrix[-1]))
         done = False
         sand_point = Point(500, 0)
         while not done:
-            self.count += 1
-            self.sand_history.clear()
             done = self.drop(sand_point)
-
-        return len(self.sand_points)
+        return self.sand_count()
 
     def get_day(self) -> str:
         return os.path.basename(__file__)[3:5]
